f1980f1b12a3eb4b2083c24258104cb5d45896fb
dmcclung <35938+dmcclung@users.noreply.github.com>
2023-12-10 21:01:48 -0600

    testing functions

--- hello.c
+++ hello.c
@@ -1,5 +1,15 @@
 #include <stdio.h>
 
-int main(void) {
-    return printf("Hello, I'm executing ARM64 instructions!\n");
+int add3(int i, int j, int k) {
+    return i + j + k;
+}
+
+int main() {
+    int i = 12;
+    int j = 10;
+    int k = 22;
+
+    int result = add3(i, j, k);
+
+    printf("add3(%i, %i, %i) = %i\n", i, j, k, result);
 }
