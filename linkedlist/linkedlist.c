d7eaaed890ccf2a12c7677f69f0dfaab7af8c9e4
dmcclung <35938+dmcclung@users.noreply.github.com>
2023-12-13 20:59:07 -0600

    refreshing linked list

--- linkedlist/linkedlist.c
+++ linkedlist/linkedlist.c
@@ -6,6 +6,35 @@ struct node {
     struct node* next;
 };
 
+void print_linked_list(struct node* ptr) {
+    while(ptr != NULL) {        
+        printf("%i\n", ptr->value);
+        ptr = ptr->next;
+    }
+}
+
 int main() {
-    
+    struct node head;
+    head.value = -1;
+
+    struct node* headPtr = &head;
+
+    for (int i = 0; i < 10; i++) {
+        struct node* next = malloc(sizeof(struct node));
+        next->value = i;
+        next->next = NULL;
+
+        headPtr->next = next;
+        headPtr = next;
+    }
+
+    print_linked_list(&head);
+
+    struct node* current = head.next;
+    struct node* temp;
+    while (current != NULL) {
+        temp = current->next;
+        free(current);
+        current = temp;
+    }
 }
