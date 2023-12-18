edeb15773125986f7685829ad57aee467918c5fd
dmcclung <35938+dmcclung@users.noreply.github.com>
2023-12-17 21:02:31 -0600

    Initialize SDL2 Go program

---
+++ sdl/go.mod
@@ -0,0 +1,5 @@
+module github.com/dmcclung/arm-riscv-c/sdl
+
+go 1.21.3
+
+require github.com/veandco/go-sdl2 v0.4.36
---
+++ sdl/main.go
@@ -0,0 +1,7 @@
+package main
+
+import "github.com/veandco/go-sdl2/sdl"
+
+func main() {
+	sdl.Init(sdl.INIT_EVERYTHING)
+}
