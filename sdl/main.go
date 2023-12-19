48492dcdf2df383863e01fcf2b3c192ac3c4b646
dmcclung <35938+dmcclung@users.noreply.github.com>
2023-12-18 22:38:24 -0600

    sdl window created

--- sdl/main.go
+++ sdl/main.go
@@ -3,5 +3,27 @@ package main
 import "github.com/veandco/go-sdl2/sdl"
 
 func main() {
-	sdl.Init(sdl.INIT_EVERYTHING)
+	if err := sdl.Init(sdl.INIT_EVERYTHING); err != nil {
+		panic(err)
+	}
+	defer sdl.Quit()
+
+	window, err := sdl.CreateWindow("Game", sdl.WINDOWPOS_UNDEFINED, sdl.WINDOWPOS_UNDEFINED,
+		800, 600, sdl.WINDOW_SHOWN)
+	if err != nil {
+		panic(err)
+	}
+	defer window.Destroy()
+
+	running := true
+	for running {
+		for event := sdl.PollEvent(); event != nil; event = sdl.PollEvent() {
+			switch event.(type) {
+			case *sdl.QuitEvent:
+				println("Done")
+				running = false				
+			}
+		}
+	}
 }
+
